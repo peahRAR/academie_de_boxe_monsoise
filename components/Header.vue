8609f3971daab92991fafc31e425b5351cd22f54
peahRAR <padecallonne@gmail.com>
2025-06-01 15:27:02 +0200

    ajout du logo du club

--- components/Header.vue
+++ components/Header.vue
@@ -8,9 +8,9 @@
     <div class="max-w-screen-xl mx-auto px-4 py-3 flex items-center">
       <nuxt-link to="/">
         <img
-          src="/img/logo.svg"
+          src="/img/logo.png"
           alt="Logo"
-          class="h-24 py-2 w-auto block"
+          class="h-32 py-2 w-auto block"
         />
       </nuxt-link>
     </div>
