d5d779dbd62b19d251acaa7ccd5f59dbbf0f7af4
peahRAR <padecallonne@gmail.com>
2025-06-05 12:04:25 +0200

    add button

--- components/NosEntrainement.vue
+++ components/NosEntrainement.vue
@@ -21,25 +21,45 @@
             <div
                 class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-full max-w-6xl text-sm uppercase font-bebas mt-8">
                 <div class="space-y-3 text-center flex-col">
-                    <Icon name="box:punching-bag" class=" mx-auto" color="white" size="60" />
+                    <Icon name="box:punching-bag" class="mx-auto" color="white" size="60" />
                     <h3 class="text-lg m-0">Boxe Loisir</h3>
                     <p class="font-body text-regular uppercase">Ludique et idéal pour brûler des calories</p>
+                    <NuxtLink to="/cours?type=loisir"
+                        class="inline-block px-4 py-2 bg-slate-800 text-white rounded-md hover:bg-red-800 transition duration-300">
+                        En savoir plus
+                    </NuxtLink>
                 </div>
+
                 <div class="space-y-3 text-center flex-col">
-                    <Icon name="box:gloves" class=" mx-auto " color="white" size="60" />
+                    <Icon name="box:gloves" class="mx-auto" color="white" size="60" />
                     <h3 class="text-lg m-0">Boxe Amateur</h3>
                     <p class="font-body text-regular uppercase">Intense et idéale pour se dépasser en compétition</p>
+                    <NuxtLink to="/cours?type=amateur"
+                        class="inline-block px-4 py-2 bg-slate-800 text-white rounded-md hover:bg-red-800 transition duration-300">
+                        En savoir plus
+                    </NuxtLink>
                 </div>
+
                 <div class="space-y-3 text-center flex-col">
-                    <Icon name="box:mouthguard" class=" mx-auto" color="white" size="60" />
+                    <Icon name="box:mouthguard" class="mx-auto" color="white" size="60" />
                     <h3 class="text-lg m-0">Boxe Educative Assault</h3>
                     <p class="font-body text-regular uppercase">Accessible et parfaite pour apprendre en sécurité</p>
+                    <NuxtLink to="/cours?type=educative"
+                        class="inline-block px-4 py-2 bg-slate-800 text-white rounded-md hover:bg-red-800 transition duration-300">
+                        En savoir plus
+                    </NuxtLink>
                 </div>
+
                 <div class="space-y-3 text-center flex-col">
-                    <Icon name="box:female-boxer" class=" mx-auto" color="white" size="60" />
+                    <Icon name="box:female-boxer" class="mx-auto" color="white" size="60" />
                     <h3 class="text-lg m-0">Ladies Boxing</h3>
                     <p class="font-body text-regular uppercase">Entre femmes, en confiance et sans pression</p>
+                    <NuxtLink to="/cours?type=ladies"
+                        class="inline-block px-4 py-2 bg-slate-800 text-white rounded-md hover:bg-red-800 transition duration-300">
+                        En savoir plus
+                    </NuxtLink>
                 </div>
+
             </div>
         </div>
     </section>
